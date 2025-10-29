1eb26a81526257fd341a9b2086f0e2b4e20f2611
Tomek Wenzel <tomek.wenzel@swisstph.ch>
2025-10-29 09:33:10 +0100

    fix: no css tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AICockpit from "./bomber.jsx";
-import "./index.css"; // optional, falls du Tailwind oder eigenes CSS willst
 
 ReactDOM.createRoot(document.getElementById("root")).render(<AICockpit />);
